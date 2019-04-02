3da94e0d42c46f057e251fa88546ce63d115cb86
Li Xueli <lixueli@qiniu.com>
2019-04-02 16:50:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const os = require('os');
 console.log("Kubia server starting...");
 
 var handler = function(request, response) {
-  console.log("Received request from " + request.connection.remoteAddress);
+  console.log("Received request from " + request.connection.remoteAddress + "!");
   response.writeHead(200);
   response.end("You've hit " + os.hostname() + "\n");
 };
